386917e86d5ae750a15909a7739e524a8a55569b
Paige Hetherington <herbmama@gmail.com>
2016-03-09 12:10:31 -0500

    added init method

--- src/main/java/com/theironyard/GameTrackerController.java
+++ src/main/java/com/theironyard/GameTrackerController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -18,6 +19,12 @@ public class GameTrackerController {
 
     @Autowired
     UserRepository users;
+
+    @PostConstruct //to run right away
+    public void init() {
+        System.out.println("Started up!");
+
+    }
 
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
